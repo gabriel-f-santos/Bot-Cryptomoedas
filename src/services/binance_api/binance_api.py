1900f6a744accd95f5e5daaeac746c7e0ba9b87c
gabrile-f-santos <gabriel-f-santos@github.com>
2022-08-26 20:03:02 -0300

    fix binance api client

--- src/services/binance_api/binance_api.py
+++ src/services/binance_api/binance_api.py
@@ -2,5 +2,8 @@
 from binance import Client
 
 def conexao_binance_api(api_key, secret_key, tld=None, test=True):
-    client = Client(api_key, secret_key, tld=tld, testnet=test)
+    if test:
+        client = Client(api_key, secret_key, tld=tld, testnet=test)
+    else:
+        client = Client(api_key, secret_key)
     return client
